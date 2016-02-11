5a043c98c3ed94774e78956a8230b246254af08d
Bo Yuan <byuan@DL11-L4.ampco.com.au>
2016-02-11 17:19:41 +1100

    stock services

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,9 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('mystock', [
+    'ionic', 
+    'mystock.controllers',
+    'mystock.services',
+    'mystock.filters',
+    'mystock.directives'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -25,49 +24,32 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+      .state('app', {
+          url: '/app',
+          abstract: true,
+          templateUrl: 'templates/menu.html',
+          controller: 'AppCtrl'
+      })
+      
+      .state('app.mystocks', {
+          url: '/mystocks',
+          views: {
+              'menuContent': {
+                  templateUrl: 'templates/mystocks.html',
+                  controller: 'MyStocksCtrl'
+              }
+          }
+      })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
+      .state('app.mystock', {
+          url: '/:stockTicker',
+          views: {
+              'menuContent': {
+                  templateUrl: 'templates/mystock.html',
+                  controller: 'MyStockCtrl'
+              }
+          }
+      });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/mystocks');
 });
