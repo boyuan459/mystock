076213760b8d4e6bcf130b711d7b577e7ad0a946
user <Admin@192-168-1-109.tpgi.com.au>
2016-02-13 21:46:39 +1100

    add search functionality

--- www/js/services.js
+++ www/js/services.js
@@ -341,4 +341,77 @@ angular.module('mystock.services', [])
     }
 })
 
+.service('modalService', function($ionicModal) {
+    
+    this.openModal = function(id) {
+        
+        var _this = this;
+        
+        if (id == 1) {
+            $ionicModal.fromTemplateUrl('templates/search.html', {
+                scope: null,
+                controller: 'SearchCtrl'
+            }).then(function(modal) {
+                _this.modal = modal;
+                _this.modal.show();
+            });
+        } else if (id == 2) {
+            $ionicModal.fromTemplateUrl('templates/login.html', {
+                scope: $scope
+            }).then(function(modal) {
+                $scope.modal = modal;
+            });
+        
+        } else if (id == 3) {
+            $ionicModal.fromTemplateUrl('templates/login.html', {
+                scope: $scope
+            }).then(function(modal) {
+                $scope.modal = modal;
+            });
+        }
+        
+    };
+    
+    this.closeModal = function() {
+        
+        var _this = this;
+        
+        if (!_this.modal) return;
+        _this.modal.hide();
+        _this.modal.remove();
+    };
+    
+    // return this;
+})
+
+.factory('searchService', function($q, $http) {
+    
+    return {
+        
+        search: function(query) {
+            
+            var deferred = $q.defer(),
+            
+            url = 'https://s.yimg.com/aq/autoc?query=' + query + '&region=CA&lang=en-CA&callback=YAHOO.util.ScriptNodeDataSource.callbacks';
+            
+            YAHOO = window.YAHOO = {
+                util: {
+                    ScriptNodeDataSource: {}
+                }
+            };
+            
+            YAHOO.util.ScriptNodeDataSource.callbacks = function(data) {
+                // console.log(data);
+                var jsonData = data.ResultSet.Result;
+                deferred.resolve(jsonData);
+            };
+            
+            $http.jsonp(url)
+            .then(YAHOO.util.ScriptNodeDataSource.callbacks);
+            
+            return deferred.promise;
+        }
+    };
+})
+
 ;
